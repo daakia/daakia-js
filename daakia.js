32cd46c5fc73d928d248dd56e0748f18d9a74df7
Piyush Mishra <piyush@codeitout.com>
2016-11-18 10:21:55 +0530

    Use an array buffer to send messages.
    Next steps:
    * Make the array buffer size configurable.

--- daakia.js
+++ daakia.js
@@ -5,56 +5,14 @@
  * @param {Parser} Parser
  */
 
-function Daakia (url, Router, Client, Transport) {
+function Daakia (url, Middleware, Client, Transport) {
   if(!this instanceof Daakia) {
     throw Error("Daakia called without the new keyword")
   }
   var tp = Transport || WebSocketTransport;
 
   this.transport = new tp(url);
-  new Router(Client,this.transport);
+  new Middleware(Client,this.transport);
 }
-
-/**
- * Payload
- * @constructor
- * @param {Uint8Array} message
- */
-
-function Payload(message) {
-   if(!this instanceof Payload) {
-    throw Error("Payload called without the new keyword")
-  }
-  this._bb = message;
-}
-
-Payload.prototype.Buffer = function () {
-  return this._bb;
-};
-
-Payload.prototype.Method = function() {
-  return this._bb[0];
-};
-
-Payload.prototype.MutMethod = function(byte) {
-  this._bb[0] = byte;
-};
-
-Payload.prototype.ReqId = function() {
-  return this._bb.slice(1,5);
-};
-Payload.prototype.MutReqId = function(value) {
-  this._bb[1] = value;
-  this._bb[2] = value >> 8;
-  this._bb[3] = value >> 16;
-  this._bb[4] = value >> 24;
-};
-
-Payload.prototype.Data = function() {
-  return this._bb.slice(5);
-};
-
-
 
 this.Daakia = Daakia;
-this.Payload = Payload;
--- websocket.js
+++ websocket.js
@@ -1,13 +1,15 @@
 /**
- * A Websocket Listener
+ * A Websocket Connection
  * @constructor
  * @param {string} url
  */
 function WebSocketTransport(url) {
-  if(!this instanceof WebSocketTransport) {
-    throw Error("Websocket Transport called without the new keyword")
+  if (!this instanceof WebSocketTransport) {
+    throw TypeError("Failed to construct 'WebSocketTransport': Please use the 'new' operator")
   }
   this._url = url;
+  this.buffer = new Uint8Array(100000);
+  this.offset = 0;
   this._attempts = 0;
 }
 /**
@@ -17,46 +19,83 @@ function WebSocketTransport(url) {
  */
 WebSocketTransport.prototype._ws = null;
 WebSocketTransport.prototype._on_connect_func = null;
-WebSocketTransport.prototype.Connect = function() {
+WebSocketTransport.prototype.Connect = function () {
   this._connect();
+};
+WebSocketTransport.prototype._send = function () {
+  if (this._ws && this._ws.readyState == this._ws.OPEN && this.offset > 0) {
+    this._ws.send(this.buffer.subarray(0,this.offset));
+    this.offset = 0;
+  }
 };
 WebSocketTransport.prototype._connect = function () {
   var self = this;
+  this.closed = false;
   this._ws = null;
   this._ws = new WebSocket(this._url);
   this._ws.binaryType = "arraybuffer";
 
   var onclose = function () {
-    var delay = (Math.pow(2, self._attempts) - 1)*1000;
-    setTimeout(self._connect,delay);
+    if(!self.closed) {
+      var delay = (Math.pow(2, self._attempts) - 1) * 1000;
+      setTimeout(self._connect.bind(self), delay);
+    }
   };
+  this._sender = null;
   this._ws.onclose = onclose;
   this._ws.onerror = onclose;
-  this._ws.onopen = function() {
-    self._on_connect_func = self._on_connect_func || function(){};
+  this._ws.onopen = function () {
+    self._on_connect_func = self._on_connect_func || function () {};
     self._attempts = 0;
+    self._sender = setInterval(self._send.bind(self), 10);
     self._on_connect_func();
   };
 
   this._ws.onmessage = this._on_message;
-  if(this._attempts <8) {
-      this._attempts++;
+  if (this._attempts < 8) {
+    this._attempts++;
   }
 };
 
-
-WebSocketTransport.prototype.OnMessage = function(on_msg_func){
-  this._on_message = on_msg_func;
-  this._ws.onmessage = on_msg_func
-
+WebSocketTransport.prototype.close = function() {
+  this._send();
+  this.closed = true;
+  if(this._sender) {
+    clearInterval(this._sender);
+    this._sender = null;
+  }
+  this._ws.close();
 };
 
-WebSocketTransport.prototype.OnConnect = function(on_connect_func) {
+
+WebSocketTransport.prototype._on_message = function (message) {
+  this._next(message.data);
+};
+
+WebSocketTransport.prototype.Next = function (on_msg_func) {
+  this._next = on_msg_func;
+};
+
+WebSocketTransport.prototype.OnConnect = function (on_connect_func) {
   this._on_connect_func = on_connect_func;
 };
 
-WebSocketTransport.prototype.Send = function(message) {
-  this._ws.send(message);
+WebSocketTransport.prototype.Send = function (header, payload) {
+  var total_length = header.byteLength + payload.byteLength;
+  var new_offset = this.offset + total_length + 4;
+  if(new_offset >= this.buffer.byteLength) {
+    this._send();
+    new_offset = this.offset+total_length+4;
+  }
+  var buf = this.buffer.subarray(this.offset,new_offset,this.buffer.byteLength);
+
+  var dv = new DataView(buf.buffer,this.offset,4);
+  this.offset = new_offset;
+  dv.setUint32(0, total_length, true);
+  buf.set(header, 4);
+
+  buf.set(payload, 4+header.byteLength);
+
 };
 
 this.WebSocketTransport = WebSocketTransport;
